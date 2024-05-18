48e21669501b05c7088cd2a459d1a813409255c8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 21:39:07 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("--- Entrada Válida ---");
+
+int n;
+Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+while(n != 0 && n >= 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    
+}
+
+if(n == 0)
+{
+    Console.WriteLine("\nOperação cancelada.");
+}
+else if(n >= 1 && n <= 9)
+{
+    Console.WriteLine($"\nNúmero selecionado = {n}");
+}
+
+Console.WriteLine("\nVolte Sempre!");
